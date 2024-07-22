fe2320fdba68e82f229f1a74ee14ccba20c91b1b
Azkabanrito1 <sulaimonhassan500@gmail.com>
2024-07-22 08:57:52 +0100

    escapulation

---
+++ src/MortageCalculator.java
@@ -0,0 +1,39 @@
+public class MortageCalculator {
+
+
+    public MortageCalculator(int principal,float annualInterest,byte years) {
+    }
+
+
+
+    public static double calculateBalance(
+            final  int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade
+    ) {
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
+
+        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+}
